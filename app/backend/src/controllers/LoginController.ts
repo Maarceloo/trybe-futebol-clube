980354b8b633817ff42ed2b67113c1755a330df2
Marcelo De Lima <delima_marcelo@outlook.com>
2022-10-24 17:03:25 -0300

    feat: Login Controller

---
+++ app/backend/src/controllers/LoginController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { jwtCreate } from '../middlewares/jwt';
+import LoginService from '../services/LoginService';
+
+class LoginController {
+  public service: LoginService;
+
+  constructor() {
+    this.service = new LoginService();
+  }
+
+  public Login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const userToken = await this.service.login({ email, password });
+
+    if (!userToken) return res.status(401).json({ message: 'Incorrect email or password' });
+
+    const { id, username, role } = userToken;
+    const token = jwtCreate({ id, username, role });
+
+    return res.status(200).json({ token });
+  };
+
+  public Validate = async (req: Request, res: Response) => {
+    console.log(req.body);
+    const { role } = req.body;
+    return res.status(200).json({ role });
+  };
+}
+
+export default LoginController;

16871a9c7603590a81622e9fc7b6b518ca226a9b
Marcelo De Lima <delima_marcelo@outlook.com>
2022-10-25 18:55:12 -0300

    Feat: middleware para insercao de matche

--- app/backend/src/routes/Matches.routes.ts
+++ app/backend/src/routes/Matches.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { jwtValidate } from '../middlewares/jwt';
 import MatchesController from '../controllers/MatchesController';
+import teamsValidate from '../middlewares/Matche.validate';
 
 const matchesController = new MatchesController();
 const matchesRouter = Router();
 
 matchesRouter.get('/', matchesController.getAllMatches);
-matchesRouter.post('/', jwtValidate, matchesController.matcheSaveInProgress);
+matchesRouter.post('/', jwtValidate, teamsValidate, matchesController.matcheSaveInProgress);
 matchesRouter.patch('/:id/finish', matchesController.changeMatcheInprogrees);
 
 export default matchesRouter;

fb5a09302d96474c14192b40827d66587652e02e
Marcelo De Lima <delima_marcelo@outlook.com>
2022-10-24 21:21:40 -0300

    Feat: Criando teste para login

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,54 @@
+import * as sinon from "sinon";
+import * as chai from "chai";
+import * as bcrypt from "bcryptjs";
+
+// import bcrypt = require('bcryptjs')
+// @ts-ignore
+import chaiHttp = require("chai-http");
+
+import { app } from "../app";
+import users from "../database/models/users";
+import userMock from "./mocks/user.mock";
+
+import { Response } from "superagent";
+import { iRegexp } from "sequelize/types/lib/operators";
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe("testes que cubrem o arquivo de /Login", () => {
+  beforeEach(async () => {
+    sinon.stub(users, "findOne").resolves({ ...userMock } as users);
+    sinon.stub(bcrypt, "compare").resolves(true);
+  });
+
+  afterEach(() => {
+    (users.findOne as sinon.SinonStub).restore();
+    (bcrypt.compare as sinon.SinonStub).restore();
+  });
+
+  it("testando login Valido", async () => {
+    const email = userMock.email;
+    const password = userMock.password;
+    const result = await chai
+      .request(app)
+      .post("/login")
+      .send({ email, password });
+
+    expect(result.status).to.be.equal(200);
+    expect(result.body).have.property("token");
+  });
+
+  it("testando login  invalido", async () => {
+    const email = "";
+    const password = "";
+    const result = await chai
+      .request(app)
+      .post("/login")
+      .send({ email, password });
+    
+    expect(result.status).to.be.equal(400);
+    expect(result.text).to.be.deep.equal('{"message":"All fields must be filled"}');
+  });
+});
---
+++ app/backend/src/tests/mocks/user.mock.ts
@@ -0,0 +1,9 @@
+const userMock = {
+    id: 10,
+    username: 'Marcelo',
+    role: 'admin',
+    email:'admin@admin.com',
+    password: 'senhaSuperSecreta',
+}
+
+export default userMock

7763b2c753546a24f00d411bac0db044d253bfd6
Marcelo De Lima <delima_marcelo@outlook.com>
2022-10-25 18:23:47 -0300

    feat: criando matches service

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,5 +1,6 @@
 import teams from '../database/models/teams';
 import matches from '../database/models/matches';
+import IMatcheCreate from '../interfaces/Matche';
 
 class MatchesService {
   public matchesGetAll = async () => {
@@ -18,6 +19,11 @@ class MatchesService {
       ],
     });
     return allMatches;
+  };
+
+  public saveMatcheInProgress = async (body: IMatcheCreate) => {
+    const newMatche = await matches.create(body);
+    return newMatche;
   };
 }
 

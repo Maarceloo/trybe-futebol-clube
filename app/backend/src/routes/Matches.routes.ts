5ca32ce735c83f6517b0d45f7553d4193b0257a1
Marcelo De Lima <delima_marcelo@outlook.com>
2022-10-25 18:23:29 -0300

    Fix: corrigindo rota

--- app/backend/src/routes/Matches.routes.ts
+++ app/backend/src/routes/Matches.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { jwtValidate } from '../middlewares/jwt';
 import MatchesController from '../controllers/MatchesController';
 
-const teamController = new MatchesController();
+const matchesController = new MatchesController();
 const matchesRouter = Router();
 
-matchesRouter.get('/', teamController.getAllMatches);
+matchesRouter.get('/', matchesController.getAllMatches);
+matchesRouter.post('/', jwtValidate, matchesController.matcheSaveInProgress);
 
 export default matchesRouter;

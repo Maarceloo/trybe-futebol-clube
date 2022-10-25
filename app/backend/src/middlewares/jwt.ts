bde8cbd97b985b5c1fccf3c017020bb982379c2c
Marcelo De Lima <delima_marcelo@outlook.com>
2022-10-25 19:43:57 -0300

    Fix: corrigindo mensagem de erro

--- app/backend/src/middlewares/jwt.ts
+++ app/backend/src/middlewares/jwt.ts
@@ -18,9 +18,9 @@ const jwtValidate = (req: Request, res: Response, next: NextFunction) => {
   try {
     const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
     req.body.user = decoded;
-    return next();
+    next();
   } catch (error) {
-    return res.status(401).json({ message: 'Invalid token' });
+    res.status(401).json({ message: 'Token must be a valid token' });
   }
 };
 

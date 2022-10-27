42410a17676666730e9e4ba3b6cc84b0e7c7d665
Marcelo De Lima <delima_marcelo@outlook.com>
2022-10-27 15:52:18 -0300

    feat: interface matche

--- app/backend/src/interfaces/Matche.ts
+++ app/backend/src/interfaces/Matche.ts
@@ -4,7 +4,22 @@ interface IMatcheCreate {
   awayTeam: number;
   homeTeamGoals: number;
   awayTeamGoals: number;
-  inProgress: boolean
+  inProgress: boolean;
 }
 
-export default IMatcheCreate;
+interface IMatcheBoard {
+  id: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+  teamHome?: {
+    teamName: string;
+  };
+  teamAway?: {
+    teamName: string;
+  };
+}
+
+export { IMatcheCreate, IMatcheBoard };
